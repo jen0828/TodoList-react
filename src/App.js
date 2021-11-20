62733495eb69273066f797c010b4e985ec4d4741
Jenny Moon <jenny0moon@gmail.com>
2021-11-20 17:10:16 +0000

    Add TodoPage.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Banner from './components/Banner/Banner';
 import { Switch, Route } from "react-router-dom"
+import TodoPage from './pages/TodoPage/TodoPage';
 
 function App() {
   return (
     <div className="App">
       <Banner />
       <Switch>
-        
+        <Route strict exact path="/" component={TodoPage}/>
       </Switch>
     </div>
   );
