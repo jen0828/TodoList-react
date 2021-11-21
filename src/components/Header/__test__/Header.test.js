dda857f136c5117e14cb0e0d78a7c16a0081fe80
Jenny Moon <jenny0moon@gmail.com>
2021-11-21 17:32:04 +0000

    Refactor tests

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,26 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-it ('should render same text passed into title prop', async () => {
-  render(<Header title="My Header"/>);
-  const headingElement = screen.getByText(/my header/i);
-  expect(headingElement).toBeInTheDocument();
-});
+describe("Header", () => {
+  it ('should render same text passed into title prop', async () => {
+    render(<Header title="My Header"/>);
+    const headingElement = screen.getByText(/my header/i);
+    expect(headingElement).toBeInTheDocument();
+  });
 
-it ('should render same text passed into title prop', async () => {
-  render(<Header title="My Header"/>);
-  const headingElement = screen.getByRole("heading");
-  expect(headingElement).toBeInTheDocument();
-});
+  it ('should render same text passed into title prop', async () => {
+    render(<Header title="My Header"/>);
+    const headingElement = screen.getByRole("heading");
+    expect(headingElement).toBeInTheDocument();
+  });
 
-it ('should render same text passed into title prop', async () => {
-  render(<Header title="My Header"/>);
-  const headingElement = screen.getByTitle("Header");
-  expect(headingElement).toBeInTheDocument();
-});
+  it ('should render same text passed into title prop', async () => {
+    render(<Header title="My Header"/>);
+    const headingElement = screen.getByTitle("Header");
+    expect(headingElement).toBeInTheDocument();
+  });
 
-it ('should render same text passed into title prop', async () => {
-  render(<Header title="My Header"/>);
-  const headingElement = await screen.findByText(/my header/i);
-  expect(headingElement).toBeInTheDocument();
+  it ('should render same text passed into title prop', async () => {
+    render(<Header title="My Header"/>);
+    const headingElement = await screen.findByText(/my header/i);
+    expect(headingElement).toBeInTheDocument();
+  });
 });
