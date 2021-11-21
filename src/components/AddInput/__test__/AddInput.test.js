fe9e86b0a98a12f701a30a221f07b266d8acd9f5
Jenny Moon <jenny0moon@gmail.com>
2021-11-21 17:52:46 +0000

    Add new test using fireEvent

--- src/components/AddInput/__test__/AddInput.test.js
+++ src/components/AddInput/__test__/AddInput.test.js
@@ -1,15 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import AddInput from "../AddInput"
 
 const mockedSetTodo = jest.fn();
 
 it('should render input element', () => {
-    render(
-        <AddInput 
-            todos={[]}
-            setTodos={mockedSetTodo}
-        />
-    );
-    const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-    expect(inputElement).toBeInTheDocument();
+  render(
+      <AddInput 
+          todos={[]}
+          setTodos={mockedSetTodo}
+      />
+  );
+  const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+  expect(inputElement).toBeInTheDocument();
+});
+
+it('should be able to type into input', () => {
+  render(
+      <AddInput 
+          todos={[]}
+          setTodos={mockedSetTodo}
+      />
+  );
+  const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
+  fireEvent.click(inputElement)
+  fireEvent.change(inputElement, { target: { value: "Practice React" } })
+  expect(inputElement.value).toBe("Practice React");
 });
