694fb7271e7adeb86fea7518382d7a305eb39b96
Jenny Moon <jenny0moon@gmail.com>
2021-11-21 13:02:09 +0000

    Add test for TodoFooter

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -6,3 +6,9 @@ it ('should render the correct amount of incomplete tasks', async () => {
   const paragraphElement = screen.getByText(/5 tasks left/i);
   expect(paragraphElement).toBeInTheDocument();
 });
+
+it ('should render "task" when the number of incomplete taks is one', async () => {
+  render(<TodoFooter numberOfIncompleteTasks={1}/>);
+  const paragraphElement = screen.getByText(/ task left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
