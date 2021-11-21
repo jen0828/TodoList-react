1279905b0617ad192ede6bc27e3af7908fd41d6d
Jenny Moon <jenny0moon@gmail.com>
2021-11-21 17:28:10 +0000

    Update test using describe block

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -1,14 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import TodoFooter from '../TodoFooter';
 
-it ('should render the correct amount of incomplete tasks', async () => {
-  render(<TodoFooter numberOfIncompleteTasks={5}/>);
-  const paragraphElement = screen.getByText(/5 tasks left/i);
-  expect(paragraphElement).toBeInTheDocument();
+describe("TodoFooter", ()=> {
+  it ('should render the correct amount of incomplete tasks', async () => {
+    render(<TodoFooter numberOfIncompleteTasks={5}/>);
+    const paragraphElement = screen.getByText(/5 tasks left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  it ('should render "task" when the number of incomplete taks is one', async () => {
+    render(<TodoFooter numberOfIncompleteTasks={1}/>);
+    const paragraphElement = screen.getByText(/ task left/i);
+    expect(paragraphElement).toBeInTheDocument();
+  });
 });
 
-it ('should render "task" when the number of incomplete taks is one', async () => {
-  render(<TodoFooter numberOfIncompleteTasks={1}/>);
-  const paragraphElement = screen.getByText(/ task left/i);
-  expect(paragraphElement).toBeInTheDocument();
-});
