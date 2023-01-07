b29e17503525ba52203424ab24324450f44f948f
Jenny Moon <jenny0moon@gmail.com>
2023-01-07 14:26:27 +0000

    Fix type errors post ts installation

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,59 +1,49 @@
-import React from 'react'
-import TodoFooter from '../TodoFooter/TodoFooter'
-import "./TodoList.css"
-import { Props } from '../AddInput/AddInput'
-import { TodoType } from '../../TodoType'
+import React from 'react';
+import TodoFooter from '../TodoFooter/TodoFooter';
+import './TodoList.css';
+import { Props } from '../AddInput/AddInput';
 
+function TodoList({ todos, setTodos }: Props) {
+  const updateTask = (id: string) => {
+    let updatedTasks = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        return todo;
+      } else {
+        return todo;
+      }
+    });
+    setTodos(updatedTasks);
+  };
 
-function TodoList({
-    todos, setTodos
-}: Props) {
+  const calcNumberOfIncompletedTasks = () => {
+    let count = 0;
+    todos.forEach((todo) => {
+      if (!todo.completed) count++;
+    });
+    return count;
+  };
 
-    const updateTask = (id) => {
-        let updatedTasks = todos.map((todo) => {
-            if(todo.id === id) {
-                todo.completed = !todo.completed;
-                return todo
-            } else {
-                return todo
-            }
-        });
-        setTodos(updatedTasks)
-    }
-
-    const calcNumberOfIncompletedTasks = () => {
-        let count = 0;
-        todos.forEach(todo => {
-            if(!todo.completed) count++
-        })
-        return count
-    }
-
-    return (
-        <div className="todolist-container">
-            <div className="todos-container">
-                <div>
-                    {
-                        todos.map((todo, index) => (
-                            <div 
-                                className={`todo-item ${todo.completed && "todo-item-active"}`} 
-                                onClick={() => updateTask(todo.id)}
-                                key={index}
-                                data-testid="task-container"
-                            >
-                                {todo.task}
-                            </div>
-                        ))
-                    }
-                </div>
+  return (
+    <div className="todolist-container">
+      <div className="todos-container">
+        <div>
+          {todos.map((todo, index) => (
+            <div
+              className={`todo-item ${todo.completed && 'todo-item-active'}`}
+              onClick={() => updateTask(todo.id)}
+              key={index}
+              data-testid="task-container">
+              {todo.task}
             </div>
-            <div>
-                <TodoFooter 
-                    numberOfIncompleteTasks={calcNumberOfIncompletedTasks()}
-                />
-            </div>
+          ))}
         </div>
-    )
+      </div>
+      <div>
+        <TodoFooter numberOfIncompleteTasks={calcNumberOfIncompletedTasks()} />
+      </div>
+    </div>
+  );
 }
 
-export default TodoList
+export default TodoList;
