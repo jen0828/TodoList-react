a5b075c8dab89594895c6245a48df03354bfa811
Jenny Moon <jenny0moon@gmail.com>
2023-01-08 16:09:22 +0000

    Fix ts type error

--- src/components/AddInput/__test__/AddInput.test.tsx
+++ src/components/AddInput/__test__/AddInput.test.tsx
@@ -1,42 +1,31 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import AddInput from "../AddInput"
+import AddInput from '../AddInput';
 
 const mockedSetTodo = jest.fn();
 
 it('should render input element', () => {
-  render(
-      <AddInput 
-          todos={[]}
-          setTodos={mockedSetTodo}
-      />
-  );
+  render(<AddInput todos={[]} setTodos={mockedSetTodo} />);
   const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
   expect(inputElement).toBeInTheDocument();
 });
 
 it('should be able to type into input', () => {
-  render(
-      <AddInput 
-          todos={[]}
-          setTodos={mockedSetTodo}
-      />
-  );
-  const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-  fireEvent.click(inputElement)
-  fireEvent.change(inputElement, { target: { value: "Practise React" } })
-  expect(inputElement.value).toBe("Practise React");
+  render(<AddInput todos={[]} setTodos={mockedSetTodo} />);
+  const inputElement = screen.getByPlaceholderText(
+    /Add a new task here.../i
+  ) as HTMLInputElement;
+  fireEvent.click(inputElement);
+  fireEvent.change(inputElement, { target: { value: 'Practise React' } });
+  expect(inputElement.value).toBe('Practise React');
 });
 
 it('should have empty input when add button is cliked', () => {
-  render(
-      <AddInput 
-          todos={[]}
-          setTodos={mockedSetTodo}
-      />
-  );
-  const inputElement = screen.getByPlaceholderText(/Add a new task here.../i);
-  fireEvent.change(inputElement, { target: { value: "Practise React" } });
-  const buttonElement = screen.getByRole("button", { name: /Add/i});
-  fireEvent.click(buttonElement)
-  expect(inputElement.value).toBe("")
+  render(<AddInput todos={[]} setTodos={mockedSetTodo} />);
+  const inputElement = screen.getByPlaceholderText(
+    /Add a new task here.../i
+  ) as HTMLInputElement;
+  fireEvent.change(inputElement, { target: { value: 'Practise React' } });
+  const buttonElement = screen.getByRole('button', { name: /Add/i });
+  fireEvent.click(buttonElement);
+  expect(inputElement.value).toBe('');
 });
